1aded3932d5d8bdaa3935d024ff901004dcfd841
Chronarr <Lodberg@gmail.com>
2023-04-06 22:22:09 +0200

    Added feedTweet component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import React, { useReducer } from 'react'
+import FeedTweet from '@/components/FeedTweet';
 
 export default function Feed() {
     const router = useRouter();
@@ -7,7 +8,7 @@ export default function Feed() {
         router.reload()
     }
     return (
-        <div className='flex sm:ml-[65px] lg:ml-[275px] max-w-[600px] flex-grow bg-white border-l border-r h-screen'>
+        <div className='flex flex-col sm:ml-[65px] lg:ml-[275px] max-w-[600px] flex-grow bg-white border-l border-r h-screen'>
             <header className='sticky w-full h-28 border-b flex flex-col top-0 bg-opacity-60 z-50 bg-white border-gray-100'>
                 <div onClick={homeClick} className='cursor-pointer flex item-center h-1/2 w-full '>
                     <h2 className="text-xl font-semibold my-auto pl-4" >Home</h2>
@@ -15,12 +16,13 @@ export default function Feed() {
                 <div className='h-1/2 flex w-full justify-between'>
                     <div className="w-1/2 flex justify-center cursor-pointer transition-colors duration-300 hover:bg-gray-200">
                         <button className='min-w-0 header-btn border-b-4 border-blue-500'>For you</button>
-                        </div>
-                        <div className="w-1/2 flex justify-center cursor-pointer transition-colors duration-300 hover:bg-gray-200">
-                    <button className='header-btn text-gray-400'>Following</button>
+                    </div>
+                    <div className="w-1/2 flex justify-center cursor-pointer transition-colors duration-300 hover:bg-gray-200">
+                        <button className='header-btn text-gray-400'>Following</button>
                     </div>
                 </div>
             </header>
+            <FeedTweet />
         </div>
     )
 }
---
+++ src/components/FeedTweet.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import TextareaAutosize from 'react-textarea-autosize';
+import { BiImageAdd, BiPoll, BiSmile, BiCalendar, BiMap } from "react-icons/bi"
+
+export default function FeedTweet() {
+
+    return (
+        <div className='w-full border-b border-gray-200 min-h-36 my-2 px-6 flex'>
+            <div>
+                <img className='rounded-full object-cover h-12 w-12 cursor-pointer filter hover:saturate-50' src="/hacker.jpg" alt="" />
+            </div>
+            <div className='flex-1'>
+                <div className='flex items-center min-h-[56px] w-full'>
+                    <TextareaAutosize maxlength="280" className="w-full ml-4 text-xl outline-none resize-none" placeholder="What's happening?" />
+                </div>
+                <div className='h-14 ml-2 flex  mt-2 min-w-full bottom-0 justify-between  border-t border-gray-200'>
+                    <div className='flex items-center'>
+                        <BiImageAdd className='text-sky-500 h-9 w-9 p-2 rounded-full hover:bg-sky-100 cursor-pointer' />
+                        <BiPoll className='text-sky-500 h-9 w-9 p-2 rounded-full hover:bg-sky-100 cursor-pointer' />
+                        <BiSmile className='text-sky-500 h-9 w-9 p-2 rounded-full hover:bg-sky-100 cursor-pointer' />
+                        <BiCalendar className='text-sky-500 h-9 w-9 p-2 rounded-full hover:bg-sky-100 cursor-pointer' />
+                        <BiMap className='text-sky-500 h-9 w-9 p-2 rounded-full hover:bg-sky-100 cursor-pointer' />
+                    </div>
+                    <button className='mr-2 rounded-full bg-sky-300 text-white w-20 h-8 my-auto pb-1 font-bold disabled:opacity-50' >Tweet</button>
+                </div>
+            </div>
+        </div>
+    )
+}
